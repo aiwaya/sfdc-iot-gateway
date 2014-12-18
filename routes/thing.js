911d756735d412884662eda9a5a8b9038517e4eb
岩谷昭宏 <aiwaya@iwataniakihiro-no-MacBook-Pro.local>
2014-12-19 08:27:02 +0900

    init

---
+++ routes/thing.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:uid', function (req, res) {
+    res.render('thing', {pusher_key: process.env.PUSHER_KEY, uid: req.params.uid});
+});
+
+
+module.exports = router;
