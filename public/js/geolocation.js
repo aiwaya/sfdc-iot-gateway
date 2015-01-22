624e9030a9cfad56223c7e0f64593160fe243e43
岩谷昭宏 <aiwaya@iwataniakihiro-no-MacBook-Pro.local>
2015-01-22 18:45:13 +0900

    g

---
+++ public/js/geolocation.js
@@ -0,0 +1,23 @@
+function success(pos) {
+    alert('ok');
+    var crd = pos.coords;
+    lat = crd.latitude;
+    lon = crd.longitude;
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+    alert('ERROR(' + err.code + '): ' + err.message);
+};
+
+$(function() {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+    navigator.geolocation.getCurrentPosition(success, error, options);
+});
