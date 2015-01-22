52d9f7fd47d0d8aaccab1f70229305b105c672a4
岩谷昭宏 <aiwaya@iwataniakihiro-no-MacBook-Pro.local>
2015-01-22 17:50:54 +0900

    g

--- public/js/lightsensor.js
+++ public/js/lightsensor.js
@@ -1,15 +1,7 @@
 (function() {
   var lightSensorThreshold = 10;
   window.addEventListener('devicelight', function(event) {
-    $('.lightlevel').text(event.value);
-    if (event.value < lightSensorThreshold) {
-      document.querySelector('audio').play();
-      $('html').addClass('partytime');
-      $('.party').addClass('schwing');
-    } else {
-      document.querySelector('audio').pause();
-      $('.party').removeClass('schwing');
-      $('html').removeClass('partytime');
-    }
+      $('#light').text('light: ' + event.value);
+
   });
 })();
