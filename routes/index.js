56706b42116ea2287c2ce3c36aabaac7bbf345ae
岩谷昭宏 <aiwaya@iwataniakihiro-no-MacBook-Pro.local>
2015-01-23 16:26:29 +0900

    g

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var decode = require('salesforce-signed-request');
 
 /* Called from Heroku App */
 router.get('/', function (req, res) {
@@ -16,20 +15,7 @@ router.get('/cockpit/:uid', function (req, res) {
     res.render('cockpit', { pusher_key: process.env.PUSHER_KEY, uid: req.param("uid") });
 });
 
-router.post('/androidcockpit', function (req, res) {
-    console.log('uid:'+ req.body.uid);
-    console.log('post');
-
-    var json = decode('3MVG9I1kFE5Iul2AlhNVv6OdFgsCtbLAbwiYGbzc1Ep2wUHfs_mhCgMZUqTDXRKeMkzxWoiNNWm.pijppZSPA', '8470605876691228371');
-    console.log(json);
-    res.render('androidcockpit', { pusher_key: process.env.PUSHER_KEY, uid: req.body.uid });
-});
-
-router.get('/androidcockpit', function (req, res) {
-    console.log('get');
-    var json = decode('3MVG9I1kFE5Iul2AlhNVv6OdFgsCtbLAbwiYGbzc1Ep2wUHfs_mhCgMZUqTDXRKeMkzxWoiNNWm.pijppZSPA', '8470605876691228371');
-    console.log(json);
-    console.log('uid:'+ req.body.uid);
+router.get('/androidcockpit/:uid', function (req, res) {
     res.render('androidcockpit', { pusher_key: process.env.PUSHER_KEY, uid: req.param("uid") });
 });
 
