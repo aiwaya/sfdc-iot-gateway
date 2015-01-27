130d6ffd3e1de40555729b5150698814927a62e0
岩谷昭宏 <aiwaya@iwataniakihiro-no-MacBook-Pro.local>
2015-01-27 14:00:08 +0900

    g

--- routes/thing.js
+++ routes/thing.js
@@ -4,9 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/:uid', function (req, res) {
     var template_name = req.query.t;
+    console.log('params : ' + template_name);
     if(template_name == null || template_name == '') {
       template_name = 'thing';
     }
+    console.log('template is : ' + template_name);
     res.render(template_name, {pusher_key: process.env.PUSHER_KEY, uid: req.params.uid});
 });
 
